0e4c59bea2794ef85ad4a92a8fb6886809e81da0
Sawyer Bateman <sawyer@sendoutcards.com>
2015-05-10 22:35:51 -0600

    Implemented Angular Quiz Application in the project.

---
+++ Learning Center/Pages/School/classes/assets/scripts/quiz/app.js
@@ -0,0 +1,97 @@
+var app = angular.module('quizApp', []);
+
+app.directive('quiz', function(quizFactory) {
+  return {
+    restrict: 'AE',
+    scope: {},
+    templateUrl: 'quizTemplate.html',
+    link: function(scope, elem, attrs) {
+      scope.start = function() {
+        scope.id = 0;
+        scope.quizOver = false;
+        scope.inProgress = true;
+        scope.getQuestion();
+      };
+
+      scope.reset = function() {
+        scope.inProgress = false;
+        scope.score = 0;
+      }
+
+      scope.getQuestion = function() {
+        var q = quizFactory.getQuestion(scope.id);
+        if(q) {
+          scope.question = q.question;
+          scope.options = q.options;
+          scope.answer = q.answer;
+          scope.answerMode = true;
+        } else {
+          scope.quizOver = true;
+        }
+      };
+
+      scope.checkAnswer = function() {
+        if(!$('input[name=answer]:checked').length) return;
+
+        var ans = $('input[name=answer]:checked').val();
+
+        if(ans == scope.options[scope.answer]) {
+          scope.score++;
+          scope.correctAns = true;
+        } else {
+          scope.correctAns = false;
+        }
+
+        scope.answerMode = false;
+      };
+
+      scope.nextQuestion = function() {
+        scope.id++;
+        scope.getQuestion();
+      }
+
+      scope.reset();
+    }
+  }
+});
+
+//Questions
+app.factory('quizFactory', function() {
+  var questions = [
+    {
+      question: "What is the Difference Between #000 and #fff?",
+      options: ["#000 is black, and #fff is not a color.", "#000 is black and #fff is White.", "They are both the same color", "Codes are not colors."],
+      answer: 1
+    },
+    {
+      question: "When did the second world war end?",
+      options: ["1945", "1939", "1944", "1942"],
+      answer: 0
+    },
+    {
+      question: "Which was the first country to issue paper currency?",
+      options: ["USA", "France", "Italy", "China"],
+      answer: 3
+    },
+    {
+      question: "Which city hosted the 1996 Summer Olympics?",
+      options: ["Atlanta", "Sydney", "Athens", "Beijing"],
+      answer: 0
+    },
+    {
+      question: "Who invented telephone?",
+      options: ["Albert Einstein", "Alexander Graham Bell", "Isaac Newton", "Marie Curie"],
+      answer: 1
+    }
+  ];
+
+  return {
+    getQuestion: function(id) {
+      if(id < questions.length) {
+        return questions[id];
+      } else {
+        return false;
+      }
+    }
+  };
+});
