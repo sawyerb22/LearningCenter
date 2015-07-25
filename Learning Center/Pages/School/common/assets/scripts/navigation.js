a546651b90a60b7a7f14b394e5ae2228ebd1ab02
Sawyer Bateman <sawyer@sendoutcards.com>
2015-07-24 19:32:58 -0600

    checking in worked on files

---
+++ Learning Center/Pages/School/common/assets/scripts/navigation.js
@@ -0,0 +1,5 @@
+$('.menu-icon').on('click', function(event) {
+   $('.menu-top').toggleClass('menu-top-click');
+   $('.menu-middle').toggleClass('menu-middle-click');
+   $('.menu-bottom').toggleClass('menu-bottom-click');
+});
